b1b862804bcbf5cba9f0e6b1b71f854c0ef872eb
MEENAKSHI23IT079 <meenakshial@student.tce.edu>
2025-12-04 14:48:58 +0530

    Updated!

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,10 @@ const db = mysql.createConnection({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
